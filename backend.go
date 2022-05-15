410b65fe6d79607f971941c8cc412e29f4b18567
Umirinbrah <95506079+Umirinbrah@users.noreply.github.com>
2022-05-15 22:32:41 +0300

    Add files via upload

--- backend.go
+++ backend.go
@@ -2,19 +2,21 @@ package main
 
 import (
     "bytes"
-    "fmt"
-    "io"
     "io/ioutil"
     "time"
     "net/http"
     "log"
+    "github.com/aglyzov/charmap"
+    "io"
 )
 
-func Decoded(body string){
+func Decoded(body []byte) string{
+	res,_ := charmap.ANY_to_UTF8(body, "KOI8-R")
 
+	return string(res)
 }
 
-func RequestData() string {
+func RequestTrainData() string {
 	key  := "qdj239j39jt"
     resp, err := http.Post("https://inf2086.ru/trains_timetable_api/timetable",
         "", bytes.NewBuffer([]byte(key)))
@@ -26,24 +28,45 @@ func RequestData() string {
     if err != nil {
         print(err)
     }
-    return string(body)
+    return Decoded(body)
+
+}
+func RequestInAndOutData() string {
+	key  := "qdj239j39jt"
+    resp, err := http.Post("https://inf2086.ru/trains_timetable_api/updates",
+        "", bytes.NewBuffer([]byte(key)))
+    if err != nil {
+        print(err)
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        print(err)
+    }
+    return Decoded(body)
 
 }
 
 func TrainsData(resp http.ResponseWriter, req *http.Request) {
-	data := RequestData()
+	data := RequestTrainData()
 	io.WriteString(resp, data)
-	fmt.Println(resp)
+
 }
+
+func InAndOutUpdates(resp http.ResponseWriter, req *http.Request) {
+	data := RequestInAndOutData()
+	io.WriteString(resp, data)
+}
+
 func main() {
 	srv := &http.Server{
 		Addr: ":8080",
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 	}
-	RequestData()
 
 	http.HandleFunc("/trainstimetable", TrainsData)
+	http.HandleFunc("/inandoutupdates", InAndOutUpdates)
 
 	log.Fatal(srv.ListenAndServe())
 
